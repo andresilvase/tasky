ed7ec08883f15f7d245601346f06aaa2d2739176
André Felipe <academicworks.adf@gmail.com>
2025-01-08 11:07:13 -0300

    refactor: simplify task creation logic and enhance input validation in create_task component

--- lib/features/tasks/views/create_task.dart
+++ lib/features/tasks/views/create_task.dart
@@ -25,30 +25,36 @@ class _CreateTaskState extends State<CreateTask> {
     _descriptionFocus.addListener(() {
       setState(() {});
     });
+
     super.initState();
   }
 
+  Task get task {
+    return Task(
+      description: _descriptionController.text,
+      title: _titleController.text,
+      date: DateTime.now(),
+      isCompleted: false,
+      id: '',
+    );
+  }
+
   void createTask() {
-    if (!_formKey.currentState!.validate()) {
-      return;
-    } else {
-      final Task task = Task(
-        description: _descriptionController.text,
-        title: _titleController.text,
-        date: DateTime.now(),
-        isCompleted: false,
-        id: '',
-      );
+    final TaskViewModel taskViewModel = Get.find();
+    taskViewModel.addTask(task);
 
-      final TaskViewModel taskViewModel = Get.find();
-      taskViewModel.addTask(task);
+    _titleController.clear();
+    _descriptionController.clear();
 
-      _titleController.clear();
-      _descriptionController.clear();
+    _descriptionFocus.unfocus();
+    Get.back();
+  }
 
-      _descriptionFocus.unfocus();
-      Get.back();
+  String? validateTitleInput(value) {
+    if (value == null || value.isEmpty) {
+      return AppLocalizations.of(context)!.titleRequired;
     }
+    return null;
   }
 
   @override
@@ -56,15 +62,10 @@ class _CreateTaskState extends State<CreateTask> {
     final bool isDarkMode = Theme.of(Get.context!).brightness == Brightness.dark;
 
     return Container(
-      margin: EdgeInsets.only(
-        right: 40,
-        left: 40,
-        top: 16,
-      ),
+      margin: EdgeInsets.only(right: 40, left: 40, top: 16),
       child: Column(
         children: [
           _title(isDarkMode),
-          SizedBox(height: _descriptionFocus.hasFocus ? 32 : 0),
           _description(isDarkMode),
           _createButton(),
         ],
@@ -78,85 +79,115 @@ class _CreateTaskState extends State<CreateTask> {
       child: Form(
         key: _formKey,
         child: TextFormField(
-          controller: _titleController,
-          style: GoogleFonts.urbanist(
-            color: isDarkMode ? TaskiColors.paleWhite : TaskiColors.statePurple,
-            fontWeight: FontWeight.normal,
-            fontSize: 18,
-          ),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Title is required';
-            }
-            return null;
-          },
           cursorErrorColor: TaskiColors.redShade,
-          decoration: InputDecoration(
-            prefixIcon: Transform.scale(
-              scale: 1.25,
-              child: Checkbox(
-                visualDensity: VisualDensity(vertical: -4),
-                checkColor: TaskiColors.white,
-                onChanged: null,
-                value: false,
-                side: BorderSide(
-                  color: TaskiColors.mutedAzure,
-                  width: 2,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                ),
-              ),
-            ),
-            labelStyle: GoogleFonts.urbanist(
-              color: TaskiColors.mutedAzure,
-              fontWeight: FontWeight.normal,
-              fontSize: 16,
-            ),
-            labelText: AppLocalizations.of(context)!.whatsInYourMind,
-            errorStyle: GoogleFonts.urbanist(
-              color: TaskiColors.fireRed,
-              fontWeight: FontWeight.normal,
-              fontSize: 14,
-            ),
-            focusedErrorBorder: noBorder(),
-            disabledBorder: noBorder(),
-            enabledBorder: noBorder(),
-            focusedBorder: noBorder(),
-            errorBorder: activeBorder(TaskiColors.redShade),
-            border: noBorder(),
-          ),
+          decoration: _titleInputDecoration(),
+          style: _titleTextStyle(isDarkMode),
+          validator: validateTitleInput,
+          controller: _titleController,
         ),
       ),
     );
   }
 
-  Widget _description(bool isDarkMode) {
-    return TextField(
-      controller: _descriptionController,
-      focusNode: _descriptionFocus,
-      style: GoogleFonts.urbanist(
-        color: isDarkMode ? TaskiColors.paleWhite : TaskiColors.statePurple,
-        fontWeight: FontWeight.normal,
-        fontSize: 18,
-      ),
-      maxLines: 2,
-      textInputAction: TextInputAction.done,
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.edit, color: TaskiColors.mutedAzure),
-        labelStyle: GoogleFonts.urbanist(
+  InputDecoration _titleInputDecoration() {
+    return InputDecoration(
+      labelText: AppLocalizations.of(context)!.whatsInYourMind,
+      errorBorder: activeBorder(TaskiColors.redShade),
+      prefixIcon: _titlePrefixIcon(),
+      labelStyle: _titleLableStyle(),
+      errorStyle: _titleErrorStyle(),
+      focusedErrorBorder: noBorder(),
+      disabledBorder: noBorder(),
+      enabledBorder: noBorder(),
+      focusedBorder: noBorder(),
+      border: noBorder(),
+    );
+  }
+
+  Transform _titlePrefixIcon() {
+    return Transform.scale(
+      scale: 1.25,
+      child: Checkbox(
+        visualDensity: VisualDensity(vertical: -4),
+        checkColor: TaskiColors.white,
+        onChanged: null,
+        value: false,
+        side: BorderSide(
           color: TaskiColors.mutedAzure,
-          fontWeight: FontWeight.normal,
-          fontSize: 16,
+          width: 2,
         ),
-        labelText: AppLocalizations.of(context)!.addNote,
-        focusedErrorBorder: noBorder(),
-        disabledBorder: noBorder(),
-        enabledBorder: noBorder(),
-        focusedBorder: noBorder(),
-        errorBorder: noBorder(),
-        border: noBorder(),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
       ),
+    );
+  }
+
+  TextStyle _titleTextStyle(bool isDarkMode) {
+    return GoogleFonts.urbanist(
+      color: isDarkMode ? TaskiColors.paleWhite : TaskiColors.statePurple,
+      fontWeight: FontWeight.normal,
+      fontSize: 18,
+    );
+  }
+
+  TextStyle _titleLableStyle() {
+    return GoogleFonts.urbanist(
+      color: TaskiColors.mutedAzure,
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
+    );
+  }
+
+  TextStyle _titleErrorStyle() {
+    return GoogleFonts.urbanist(
+      color: TaskiColors.fireRed,
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    );
+  }
+
+  Widget _description(bool isDarkMode) {
+    return Container(
+      margin: EdgeInsets.only(top: _descriptionFocus.hasFocus ? 32 : 0),
+      child: TextField(
+        decoration: _descriptionInputDecoration(),
+        style: _descriptionTextstyle(isDarkMode),
+        textInputAction: TextInputAction.done,
+        controller: _descriptionController,
+        focusNode: _descriptionFocus,
+        maxLines: 2,
+      ),
+    );
+  }
+
+  InputDecoration _descriptionInputDecoration() {
+    return InputDecoration(
+      prefixIcon: Icon(Icons.edit, color: TaskiColors.mutedAzure),
+      labelText: AppLocalizations.of(context)!.addNote,
+      labelStyle: _descriptionLabelStyle(),
+      focusedErrorBorder: noBorder(),
+      disabledBorder: noBorder(),
+      enabledBorder: noBorder(),
+      focusedBorder: noBorder(),
+      errorBorder: noBorder(),
+      border: noBorder(),
+    );
+  }
+
+  TextStyle _descriptionTextstyle(bool isDarkMode) {
+    return GoogleFonts.urbanist(
+      color: isDarkMode ? TaskiColors.paleWhite : TaskiColors.statePurple,
+      fontWeight: FontWeight.normal,
+      fontSize: 18,
+    );
+  }
+
+  TextStyle _descriptionLabelStyle() {
+    return GoogleFonts.urbanist(
+      color: TaskiColors.mutedAzure,
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
     );
   }
 
@@ -165,7 +196,11 @@ class _CreateTaskState extends State<CreateTask> {
       margin: EdgeInsets.only(top: 16),
       alignment: Alignment.centerRight,
       child: TextButton(
-        onPressed: createTask,
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            createTask();
+          }
+        },
         child: Text(
           AppLocalizations.of(context)!.create,
           style: GoogleFonts.urbanist(
